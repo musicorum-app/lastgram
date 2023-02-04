8c7fc7e1dd98ab587a985298f78cef7532360bce
Renan 츄 <lyricalsoul@proton.me>
2023-02-04 18:57:37 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,34 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build and push Docker") {
+    host("Build and push a Docker image") {
+        dockerBuildPush {
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
+
+            // path to Docker context (by default, context is working dir)
+            context = "docker"
+            // path to Dockerfile relative to the project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+            file = "Dockerfile"
+            // build-time variables
+            args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            labels["vendor"] = "musicorum"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
+            tags {
+                // use current job run number as a tag - '0.0.run_number'
+                +"musicorum.registry.jetbrains.space/p/prjkey/mydocker/lastgram:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            }
+        }
+    }
+}
