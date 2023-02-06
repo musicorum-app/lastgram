05225f598bfea1162f715dce62f205aeeb03e12c
lyricalsoul <lyricalsoul@protonmail.com>
2023-02-06 00:16:48 -0400

    add botinfo scratch, update locales
    Signed-off-by: lyricalsoul <lyricalsoul@protonmail.com>

--- src/commandEngine/commands/all/botinfo.ts
+++ src/commandEngine/commands/all/botinfo.ts
@@ -1,7 +1,24 @@
 import { Context } from '../../../multiplatformEngine/common/context.js'
+import client from 'prom-client'
+import { isDevelopment } from '../../../utils.js'
 
-export default (ctx: Context) => {
-  ctx.reply('Hello, world!')
+export default async (ctx: Context) => {
+  const processMemory = process.memoryUsage()
+  const rss = processMemory.rss / 1024 / 1024
+  const heapTotal = processMemory.heapTotal / 1024 / 1024
+  const heapUsed = processMemory.heapUsed / 1024 / 1024
+  const totalTelegramMessages = await client.register.getSingleMetricAsString(isDevelopment ? 'lg_dev_platform_telegram_total_handled_messages' : 'lg_platform_telegram_total_handled_messages').then((s) => {
+    const v = s.split(' ')
+    return v[v.length - 1]
+  })
+
+  ctx.reply('lastgram {{mode}}\n💻 **Memory usage:** {{rss}} MB (RSS), {{heapTotal}} MB (heap total), {{heapUsed}} MB (heap used)\n📩 **{{updates}}** messages from Telegram since last restar', {
+    mode: isDevelopment ? 'development' : 'stable',
+    rss: rss.toFixed(2),
+    heapTotal: heapTotal.toFixed(2),
+    heapUsed: heapUsed.toFixed(2),
+    updates: totalTelegramMessages
+  })
 }
 
 export const info = {
