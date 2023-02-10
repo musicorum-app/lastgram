e5b779107447865fd988bbd9711a33ac543387d3
Matheus Dias <methidezeeni@gmail.com>
2023-02-10 00:12:11 -0300

    chore: modify docker build

--- .space.kts
+++ .space.kts
@@ -13,37 +13,24 @@ job("Build and push Docker") {
         }
     }
 
-    host("Get build version") {
+    job("Build and push a Docker image") {
+        kaniko {
+            beforeBuildScript {
+                content = "export BUILD_VERSION=\$(cat package.json|grep version|head -1|awk -F: '{ print \$2 }'|sed 's/[\", ]//g')"
+            }
 
-    }
+           build {
+               context = "docker"
+               dockerfile = "Dockerfile"
+               labels["vendor"] = "musicorum"
+           }
 
-    host("Build and push a Docker image") {
-        shellScript {
-            content = "export BUILD_VERSION=\$(cat package.json|grep version|head -1|awk -F: '{ print \$2 }'|sed 's/[\", ]//g')"
-        }
-        dockerBuildPush {
-            // by default, the step runs not only 'docker build' but also 'docker push'
-            // to disable pushing, add the following line:
-            // push = false
-
-            // path to Docker context (by default, context is working dir)
-            // path to Dockerfile relative to the project root
-            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-            file = "Dockerfile"
-            // build-time variables
-            // args["HTTP_PROXY"] = "http://10.20.30.2:1234"
-            // image labels
-            labels["vendor"] = "musicorum"
-            // to add a raw list of additional build arguments, use
-            // extraArgsForBuildCommand = listOf("...")
-            // to add a raw list of additional push arguments, use
-            // extraArgsForPushCommand = listOf("...")
-            // image tags
-            val repository = "musicorum.registry.jetbrains.space/p/main/containers/lastgram"
-            tags {
-                // use current job run number as a tag - '0.0.run_number'
-                +"$repository:\$BUILD_VERSION"
-                +"$repository:latest"
+            push("musicorum.registry.jetbrains.space/p/main/containers/lastgram") {
+                tags {
+                    // use current job run number as a tag - '0.0.run_number'
+                    +"\$BUILD_VERSION-\$JB_SPACE_EXECUTION_ID"
+                    +"latest"
+                }
             }
         }
     }
