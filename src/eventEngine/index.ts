8ef23c46075875655c2c12d7760842f141cdf02e
lyricalsoul <lyricalsoul@protonmail.com>
2023-02-12 17:43:18 -0400

    feat: rework eventEngine

--- src/eventEngine/index.ts
+++ src/eventEngine/index.ts
@@ -1,59 +1,46 @@
-import { AwaitingEvent, EngineEventList, EventConstraints, EventListener, isSameConstraint } from './types/engine.js'
+import { EventListenerHandler, EventListenerIdentifier } from './types/engine.js'
 import { debug } from '../loggingEngine/logging.js'
 import { MinimalContext } from '../multiplatformEngine/common/context.js'
+import { backend } from '../cachingEngine/index.js'
+
+// event key: event_<id>
+// the value is the handler function name
 
 class EventEngine {
-  private awaitingEvents: Map<string, Map<string, AwaitingEvent<any>[]>>
-
-  constructor () {
-    this.awaitingEvents = new Map()
+  extractIDFromData (data: string): [string, string] {
+    const [command, handler] = data.split('::')
+    return [command, handler]
   }
 
-  queueEvent<T extends keyof EngineEventList> (constraints: EventConstraints, event: T, listener: EventListener) {
-    debug('eventEngine.queueEvent', `queueing new event ${event} for platform ${constraints.platform}`)
-    this.addEventToQueue(constraints, event, listener)
+  queueEvent (id: string, listener: EventListenerIdentifier) {
+    debug('eventEngine.queueEvent', `queueing new event under id ${id}`)
+    this.addEventToQueue(id, listener)
   }
 
-  async dispatchEvent<T extends keyof EngineEventList> (constraints: EventConstraints, event: T, ctx: MinimalContext) {
-    const platformListeners = this.awaitingEvents.get(constraints.platform)
-    if (!platformListeners) return
-    const listeners = platformListeners.get(constraints.userID)
-    if (!listeners) return
-    const eventListeners = listeners.filter(l => l.event === event && isSameConstraint(l.constraints, constraints))
-    for (const listener of eventListeners) {
-      const result = await listener.listener(ctx)
-      if (result) {
-        debug('eventEngine.dispatchEvent', `event ${event} for platform ${constraints.platform} was handled by listener`)
-        this.removeEventFromQueue(constraints, listener)
-      }
-    }
+  async dispatchEvent (id: string, ctx: MinimalContext) {
+    const key = `event_${id}`
+    const keyValue = await backend!.get(key)
+    debug('eventEngine.dispatchEvent', `id ${id} path: ${keyValue}`)
+    if (!keyValue) return
+    const [commandName, handlerName] = keyValue.split('.')
+
+    const commandEngine = await import('../commandEngine/index.js').then(m => m.commandRunner)
+    const command = commandEngine.findCommand(commandName)!
+    const handler: EventListenerHandler = command.interactionHandlers![handlerName]
+    if (!handler) return
+
+    this.removeEventFromQueue(key)
+
+    return handler(ctx)
   }
 
-  private addEventToQueue<T extends keyof EngineEventList> (constraints: EventConstraints, event: T, listener: EventListener) {
-    const platformListeners = this.awaitingEvents.get(constraints.platform) || new Map()
-    const listeners: AwaitingEvent<T>[] = platformListeners.get(constraints.userID) || []
-    listeners.push({
-      event,
-      listener: listener as EventListener,
-      constraints
-    })
-    platformListeners.set(constraints.userID, listeners)
-    this.awaitingEvents.set(constraints.platform, platformListeners)
+  private addEventToQueue (id: string, listener: EventListenerIdentifier) {
+    const key = `event_${id}`
+    backend!.setTTL(key, `${listener.command}.${listener.handler}`, 15 * 60 * 1000).then(() => undefined)
   }
 
-  private removeEventFromQueue<T extends keyof EngineEventList> (constraints: EventConstraints, event: AwaitingEvent<T>) {
-    const platformListeners = this.awaitingEvents.get(constraints.platform) || new Map()
-    const listeners = platformListeners.get(constraints.userID) || []
-    const index = listeners.findIndex((l: AwaitingEvent<T>) => l.event === event.event && l.listener === event.listener)
-    if (index > -1) {
-      listeners.splice(index, 1)
-    }
-    platformListeners.set(constraints.userID, listeners)
-    if (listeners.length === 0) {
-      platformListeners.delete(constraints.userID)
-    }
-
-    this.awaitingEvents.set(constraints.platform, platformListeners)
+  private removeEventFromQueue (key: string) {
+    backend!.delete(key).then(() => undefined)
   }
 }
 
--- src/eventEngine/types/engine.ts
+++ src/eventEngine/types/engine.ts
@@ -1,29 +1,4 @@
 import { MinimalContext } from '../../multiplatformEngine/common/context.js'
 
-export interface EngineEvent {
-  data: string
-}
-
-export type EventListener = (ctx: MinimalContext) => Promise<boolean> | boolean
-
-export type EngineEventList = {
-  'buttonClick': undefined
-}
-
-export interface AwaitingEvent<T extends keyof EngineEventList> {
-  event: keyof EngineEventList
-  constraints: EventConstraints
-  listener: EventListener
-}
-
-export interface EventConstraints {
-  platform: string
-  userID: string
-  channelID: string
-}
-
-export const isSameConstraint = (a: EventConstraints, b: EventConstraints) => {
-  const sameUserID = a.userID === b.userID || a.userID === '*' || b.userID === '*'
-  const sameChannelID = a.channelID === b.channelID || a.channelID === '*' || b.channelID === '*'
-  return a.platform === b.platform && sameUserID && sameChannelID
-}
+export type EventListenerIdentifier = { command: string, handler: string }
+export type EventListenerHandler = (ctx: MinimalContext) => void
